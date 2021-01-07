d599d1cd40779dd918e56d4d48480e2b26aaf419
Alban <alban.manishimwe@gmail.com>
2021-01-07 15:20:39 +0300

    First Commit

---
+++ Higher_Education_Information_System/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Higher_Education_Information_System project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Higher_Education_Information_System.settings')
+
+application = get_asgi_application()
---
+++ academic_programs_information_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AcademicProgramsInformationSystemConfig(AppConfig):
+    name = 'academic_programs_information_system'
